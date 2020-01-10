db7957b8329d7da84d5c479f7b86d6c00b16a259
PremKumar <p.pushparaj@lakeba.com>
2020-01-10 17:13:21 +0530

    Test git commit

---
+++ PremJava/src/newprem.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class newprem {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		// TODO Auto-generated method stub
+		StringBuffer sb=new StringBuffer("Hello ");  
+		sb.append("Java");//now original string is changed  
+		int e=sb.codePointAt(0);
+		System.out.println(sb);//prints Hello Java
+		System.out.println(e);
+		System.out.println(sb.substring(4));
+		System.out.println(sb.reverse());
+		System.out.println(sb.lastIndexOf("Java", 0));
+		String s1="prem";
+		String s2=s1.concat("Kumar");
+		System.out.println(s2);
+	}
+
+}
